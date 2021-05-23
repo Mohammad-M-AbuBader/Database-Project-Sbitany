1d6c77603f9a90eb808b548bd3e30bbc22a5b082
AmeerEleyan <ameer.eleyan.pgr@gmail.com>
2021-05-23 18:55:40 +0300

    Amendment No. 2

---
+++ Group_5-Project/src/Controllers/GeneralManagerController.java
@@ -0,0 +1,94 @@
+/**
+ * Sample Skeleton for 'GeneralManager.fxml' Controller Class
+ */
+
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GeneralManagerController {
+
+    @FXML // fx:id="btBranches"
+    private Button btBranches; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btAccount"
+    private Button btAccount; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btLogout"
+    private Button btLogout; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btEmployee"
+    private Button btEmployee; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btCustomres"
+    private Button btCustomres; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btProducts"
+    private Button btProducts; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btSupplierBills"
+    private Button btSupplierBills; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btCustomerBills"
+    private Button btCustomerBills; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btSuppliers"
+    private Button btSuppliers; // Value injected by FXMLLoader
+
+
+    public void handleBtBranches() {
+
+    }
+
+    public void handleBtAccount() {
+
+    }
+
+    public void handleBtLogout() {
+
+    }
+
+    public void handleBtEmployee() {
+
+    }
+
+    public void handleBtCustomres() {
+
+    }
+
+    public void handleBtProducts() {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("../FXML/Products.fxml"));
+           Stage window = new Stage();
+            window.initModality(Modality.APPLICATION_MODAL);
+            window.setTitle("Products");
+            window.setScene(new Scene(root));
+            window.setResizable(false);
+            window.show();
+        }catch (IOException exception){
+
+        }
+
+    }
+
+    public void handleBtSupplierBills() {
+
+    }
+
+    public void handleBtCustomerBills() {
+
+    }
+
+    public void handleBtSuppliers() {
+
+    }
+
+}
--- Group_5-Project/src/Controllers/Main.java
+++ Group_5-Project/src/Controllers/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        root = FXMLLoader.load(getClass().getResource("../FXML/EmployeeInfo.fxml"));
+        root = FXMLLoader.load(getClass().getResource("../FXML/GeneralManager.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
